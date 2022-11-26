5adf80cb5d360867b05191f2b5b994b4d94737cd
marvelman3284 <jedimaster3284@gmail.com>
2022-11-26 12:39:06 -0500

    queues

--- src/shah/jeevan/Main.java
+++ src/shah/jeevan/Main.java
@@ -3,11 +3,13 @@ package shah.jeevan;
 public class Main {
 
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<Integer>();
-        stack.push(50);
-        stack.push(60);
-        stack.push(70);
-        System.out.println(stack.pop());
-        System.out.println(stack);
+        Queue<Double> queue = new Queue<Double>();
+        queue.enqueue(3.14);
+        queue.enqueue(4.25);
+        queue.enqueue(5.36);
+        System.out.println(queue.dequeue());
+        System.out.println(queue);
+        System.out.println(queue.front());
+        System.out.println(queue);
     }
 }
---
+++ src/shah/jeevan/Queue.java
@@ -0,0 +1,40 @@
+package shah.jeevan;
+
+public class Queue<T> extends LinkedList {
+
+    public Queue() {
+        super();
+    }
+
+    public void enqueue(T data) {
+        super.insertTail(data);
+    }
+
+    public T dequeue() {
+        return (T) super.deleteHead();
+    }
+
+    public T front() {
+        return (T) super.front();
+    }
+
+    public T back() {
+        return (T) super.back();
+    }
+
+    public boolean isEmpty() {
+        return super.isEmpty();
+    }
+
+    public int size() {
+        return super.length();
+    }
+
+    public String toString() {
+        return "Queue " + super.toString();
+    }
+
+    public Queue clone() {
+        return this;
+    }
+}
